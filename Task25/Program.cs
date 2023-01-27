aa38d64ae77a48f6383951259fc1e43687c401d7
Makc <m_stefanenkov@mail.ru>
2023-01-27 18:38:45 +0300

    Решил задачу 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите первое число A: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число B: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 < 0) number1 = number1 * -1;
+if (number2 < 1) Console.WriteLine("Некорректный ввод!");
+
+else
+{
+    int exponentiation = Expon(number1, number2);
+    Console.WriteLine($"Число A, возведенное в степень B = {exponentiation}");
+
+    int Expon(int num1, int num2)
+    {
+        int exponentiation = number1;
+        for (int i = 1; i < number2; i++)
+        {
+            exponentiation = exponentiation * number1;
+        }
+        return exponentiation;
+    }
+}
