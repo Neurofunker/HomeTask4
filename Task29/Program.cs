e1d15acc20b9496d9c52b3307182d234094e87a2
Makc <m_stefanenkov@mail.ru>
2023-01-29 10:46:22 +0300

    Допилил задачу 29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -15,14 +15,15 @@ int[] Generatearray(int size, int min, int max)
 
 void PrintArray(int[] arr)
 {
-    Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
         if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
         else Console.Write($"{arr[i]}");
     }
-    Console.Write("]");
 }
 
 int[] array = Generatearray(8, 10, 100);
 PrintArray(array);
+Console.Write(" -> [");
+PrintArray(array);
+Console.Write("]");
